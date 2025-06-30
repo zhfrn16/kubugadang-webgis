dc83393d3df0042c7efc0f50bc4820a1324c87b1
rafimp <northernosprey2013@gmail.com>
2025-06-30 04:05:42 +0000

    Removed unnecesary buttons

--- app/Views/web/detail_package.php
+++ app/Views/web/detail_package.php
@@ -153,60 +153,6 @@
                         <div class="col-7">
                             <h4 class="card-title text-end">Package Information</h4>
                         </div>
-                        <div class="col-5">
-                            <div class="text-end">
-                                <?php if (logged_in()) : ?>
-                                    <?php if (in_groups(['admin']) || in_groups(['master'])) : ?>
-                                        <button type="submit" class="btn icon btn-outline-primary" title="Extend Package" disabled><i class="fa-solid fa-plus-square"></i> Extend</button>&nbsp;&nbsp;
-                                        <button type="submit" class="btn icon btn-outline-primary" title="Custom Package" disabled><i class="fa-solid fa-puzzle-piece"></i> Custom</button>
-                                    <?php else : ?>
-                                        <div class="d-flex justify-content-end align-items-center gap-2">
-                                            <!-- Form Extend -->
-                                            <form id="customForm" action="<?= base_url('/web/detailreservation/addextend'); ?>/<?= esc($data['id']); ?>" method="post" onsubmit="checkRequired(event)" enctype="multipart/form-data">
-                                                <?= csrf_field(); ?>
-                                                <button type="submit" class="btn icon btn-outline-primary" title="Extend Package">
-                                                    <i class="fa-solid fa-plus-square"></i> Extend
-                                                </button>
-                                            </form>
-
-                                            <!-- Form Custom -->
-                                            <form id="customizeForm" action="<?= base_url('/web/detailreservation/addcustompackage'); ?>/<?= esc($data['id']); ?>" method="post" onsubmit="checkRequired(event)" enctype="multipart/form-data">
-                                                <?= csrf_field(); ?>
-                                                <button type="submit" class="btn icon btn-outline-primary" title="Custom Package">
-                                                    <i class="fa-solid fa-puzzle-piece"></i> Custom
-                                                </button>
-                                            </form>
-                                        </div>
-                                    <?php endif; ?>
-
-                                <?php else : ?>
-                                    <div class="d-flex justify-content-end align-items-center gap-2">
-                                        <!-- Button Extend -->
-                                        <button type="button" class="btn icon btn-outline-primary" title="Extend Package" onclick="redirectToLogin()">
-                                            <i class="fa-solid fa-plus-square"></i> Extend
-                                        </button>
-
-                                        <!-- Button Custom -->
-                                        <button type="button" class="btn icon btn-outline-primary" title="Custom Package" onclick="redirectToLogin()">
-                                            <i class="fa-solid fa-puzzle-piece"></i> Custom
-                                        </button>
-                                    </div>
-                                    <script>
-                                        function redirectToLogin() {
-                                            Swal.fire({
-                                                icon: 'warning',
-                                                title: 'You are not logged in',
-                                                text: 'Please log in to proceed.',
-                                                confirmButtonText: 'OK',
-                                            }).then(() => {
-                                                window.location.href = '<?= base_url('/login'); ?>';
-                                            });
-                                        }
-                                    </script>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-
                     </div>
                 </div>
 
